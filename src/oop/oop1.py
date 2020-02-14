9f7f045d566da8011b736e35f16b9d40bb7625e4
Dakotah Huey <hueydakotah@gmail.com>
2020-02-14 12:22:56 -0500

    oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,64 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:  # base class
+    # constructor
+    def __init__(self, movement, wheels, fuel):
+        self.movement = movement
+        self.wheels = wheels
+        self.fuel = fuel
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self, movement, wheels, fuel):
+        super().__init__(movement, wheels, fuel)
+        # Can also do Vehicle.__init__(self, movement, wheels, fuel)
+        self.movement = "flight"
+        self.wheels = 3
+        self.fuel = "jet fuel"
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+class Starship(FlightVehicle):
+    def __init__(self, movement, wheels, fuel, weapon):
+        super().__init__(movement, wheels, fuel)
+        self.weapon = "blaster"
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, movement, wheels, fuel, armored=False):
+        super().__init__(movement, wheels, fuel)
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+class Car(GroundVehicle):
+    def __init__(self, movement, wheels, fuel, armored=False):
+        super().__init__(movement, wheels, fuel, armored)
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, movement, wheels, fuel, armored=False):
+        super().__init__(movement, wheels, fuel, armored)
+
+    def __str__(self):
+        print(f"{self.movement}, {self.wheels}, {self.fuel}")
+
+
+# motorcycle1 = Motorcycle("ground", 2, "gasoline", armored=True)
+# print(motorcycle1.movement)
+
